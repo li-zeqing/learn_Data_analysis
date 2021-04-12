575e49b0dd7184c630ccfbadd60b247223feb489
lizeqing <lizeqing77@163.com>
2021-04-12 23:54:33 +0800

    add a test3_2.py

---
+++ u1day3_test2.py
@@ -0,0 +1,12 @@
+#numpy 读取数据
+#np.loadtxt(frame,dtype=np.float,delimiter=None,skiprows=0,usecols=None,unpack=Fals)
+#frame :文件、字符串、产生器的地址，可以是.gz或bz2压缩文件
+#dtype:数据类型，可选，csv的字符串以什么数据类型读入数组中，默认为np.float
+#delimiter:分隔字符串，默认是任何空格，改为 逗号 delimiter = ","
+#skiprows: 跳过前x行，一般跳过第一行表头
+#usecols : 读取指定的列，索引，元组类型
+#unpack : 如果True，读入属性将分为写入不同数组变量[类似矩阵的转置效果]，Flse读入数据只写入一个数组变量，默认False
+
+
+
+
